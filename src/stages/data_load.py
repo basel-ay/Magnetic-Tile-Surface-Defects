e51e24ffa41bb5eaa83319e7dabaa5a8b19b015e
Basel Ayman <baselaymansalah@gmail.com>
2023-05-04 15:49:51 +0300

    -U

--- src/stages/data_load.py
+++ src/stages/data_load.py
@@ -8,3 +8,23 @@ import argparse
 from src.utils.data_utils import dataset_prep
 from src.utils.load_params import load_params
 
+def data_load(params):
+    
+    dataset_url = params.data_load.dataset_url
+    data_dir = Path(params.data_load.data_dir)
+    data_dir.mkdir(exist_ok=True)
+    orig_dirname = params.data_load.orig_dirname
+    new_dirname = params.data_load.new_dirname
+    dataset_prep(dataset_url=dataset_url,
+                 data_dir=data_dir,
+                 orig_dirname=orig_dirname,
+                 new_dirname=new_dirname)
+
+
+if __name__ == '__main__':
+    args_parser = argparse.ArgumentParser()
+    args_parser.add_argument('--config', dest='config', required=True)
+    args = args_parser.parse_args()
+    params_path = args.config
+    params = load_params(params_path)
+    data_load(params)
